df9997c1f176b05140ddff0dbed6d161263e11ed
Evgenygroup <mailfrom61@gmail.com>
2021-04-01 09:27:45 +0200

    first commit

---
+++ src/main/java/com/evgeny/uploademo/UploademoApplication.java
@@ -0,0 +1,13 @@
+package com.evgeny.uploademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploademoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploademoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/evgeny/uploademo/UploademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evgeny.uploademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploademoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
